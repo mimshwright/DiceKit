6abb3de3402768a204711b25515462d00ab02483
Mims H. Wright <mims@mimswright.com>
2015-05-16 18:50:58 -0700

    Initial Commit

---
+++ DiceKit/DiceKit.h
@@ -0,0 +1,19 @@
+//
+//  DiceKit.h
+//  DiceKit
+//
+//  Created by Mims Wright on 5/16/15.
+//  Copyright (c) 2015 Mims Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiceKit.
+FOUNDATION_EXPORT double DiceKitVersionNumber;
+
+//! Project version string for DiceKit.
+FOUNDATION_EXPORT const unsigned char DiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiceKit/PublicHeader.h>
+
+
